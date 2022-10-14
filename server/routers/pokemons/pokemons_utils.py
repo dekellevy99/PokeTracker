e185af2acc4bc618e7f5b53d1453d59b84d58d3f
dekellevy99 <dekelllevy@gmail.com>
2022-10-14 17:52:22 +0300

    add a function to validate pokemon's type

--- server/routers/pokemons/pokemons_utils.py
+++ server/routers/pokemons/pokemons_utils.py
@@ -2,12 +2,22 @@ from Queries import queries
 from fastapi import HTTPException, status
 
 
-def validate_pokemon(pokemon_name):
+def validate_pokemon_name(pokemon_name):
     valid_pokemon_names = queries.get_all_pokemon_names()
     if pokemon_name not in valid_pokemon_names:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail={
                 "error": "invalid pokemon name."
+            }
+        )
+
+def validate_pokemon_type(pokemon_type):
+    valid_pokemon_types = queries.get_all_pokemon_types()
+    if pokemon_type not in valid_pokemon_types:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail={
+                "error": "invalid pokemon type."
             }
         )
