f675fffe921617f54d65f7343f367cafc29b7d4b
dekellevy99 <dekelllevy@gmail.com>
2022-10-16 12:45:23 +0300

    create the server

---
+++ server/server.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from routers import pokemons_api, trainers_api
+import uvicorn
+
+app = FastAPI()
+app.include_router(pokemons_api.router)
+app.include_router(trainers_api.router)
+
+
+@app.get("/")
+def root():
+    return "server is running"
+
+if __name__ == "__main__":
+    uvicorn.run("server:app", host="localhost", port=8000, reload=True)
